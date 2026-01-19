8ab84cbfa1199fca51255d9286b76e7f4e5327ef
Tom <rinuo@qq.com>
2026-01-19 14:11:54 +0800

    m

--- watch_hf.sh
+++ watch_hf.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 # 处理 SEO 任务的函数
-# run_seo() {
-#     while true; do
-#         echo "[SEO] --- 开始新一轮处理 ---"
-#         node ace ai:seo 
-#         echo "[SEO] --- 处理完成，休息 30 秒 ---"
-#         sleep 30
-#     done
-# }
+run_summary() {
+    while true; do
+        echo "[SUM] --- 开始新一轮处理 ---"
+        # node ace ai:seo 
+        node ace gemini:summary 
+        echo "[SUM] --- 处理完成，休息 501 秒 ---"
+        sleep 501
+    done
+}
 
 # 处理 GEO 任务的函数
 run_webp() {
     while true; do
-        echo "[GEO] --- 开始新一轮处理 ---"
+        echo "[WEBP] --- 开始新一轮处理 ---"
         node ace webp:run 
-        echo "[GEO] --- 处理完成，休息 660 秒 ---"
-        sleep 660
+        echo "[WEBP] --- 处理完成，休息 662 秒 ---"
+        sleep 662
     done
 }
 
 # 同时在后台启动两个任务
-# run_seo &
+run_summary &
 run_webp &
 
 # 保持主进程不退出，否则容器会关闭
